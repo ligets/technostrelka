bf5a0fad0cf9813753a9f935091ced47676754d4
Kirillica228 <kirillicar8@gmail.com>
2025-02-01 20:24:02 +0400

    Sketch home

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -25,31 +25,36 @@ export default function RootLayout({ children }) {
     };
 
     return (
-        <html lang="en" className={montserrat.variable}>
-        <body className="antialiased font-montserrat">
+        <html lang="en" className={montserrat.variable} >
+        <body className="antialiased font-montserrat w-[85%] mx-auto">
         <header className="flex items-center justify-between p-4">
                     <span className="text-[#4400BA] text-[32px] font-bold">
                         GeoTouristicService
                     </span>
 
-            <div className="flex items-center gap-2 w-[600px]">
+            <div className="flex items-center gap-2 w-[600px] relative">
+                <div className="absolute left-3">
+                    <svg width="21" height="22" viewBox="0 0 21 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M20.6511 19.5123L15.5009 14.356C18.0904 10.9856 17.8314 6.11728 14.724 3.03498C13.0265 1.33539 10.8398 0.5 8.62439 0.5C6.40895 0.5 4.22228 1.33539 2.52474 3.03498C-0.841579 6.40535 -0.841579 11.8786 2.52474 15.249C4.22228 16.9486 6.40895 17.784 8.62439 17.784C10.4658 17.784 12.3072 17.2078 13.8321 16.0268L19.0111 21.1543C19.2412 21.3848 19.5289 21.5 19.8454 21.5C20.1332 21.5 20.4496 21.3848 20.6798 21.1543C21.1114 20.7222 21.1114 19.9733 20.6511 19.5123ZM8.65316 15.4506C6.95561 15.4506 5.40193 14.7881 4.19351 13.607C1.74789 11.1584 1.74789 7.15432 4.19351 4.67696C5.37316 3.49588 6.95561 2.83333 8.65316 2.83333C10.3507 2.83333 11.9044 3.49588 13.1128 4.67696C14.3212 5.85803 14.9542 7.44239 14.9542 9.14198C14.9542 10.8416 14.2925 12.3971 13.1128 13.607C11.9332 14.8169 10.3219 15.4506 8.65316 15.4506Z" fill="#8D8D8D" />
+                    </svg>
+                </div>
                 <input
                     type="text"
                     placeholder="Искать маршруты"
-                    className="border px-3 py-2 rounded-md w-[100%]"
+                    className="border px-10 py-2  w-[100%] rounded-[25px] border-gray-400  p-2  shadow-md bg-white outline-none"
                 />
             </div>
 
             <div className="flex gap-2">
                 <button
                     onClick={() => { setIsLoginForm(true); openModal(); }}
-                    className="px-4 py-2 border border-[#4400BA] text-[#4400BA] font-bold rounded-md"
+                    className="rounded-[25px] py-[0.5em] px-[2em] bg-[#4400BA] text-white text-[15px] font-bold border-[1px] border-[#4400BA] hover:bg-transparent hover:text-indigo-800 transition-all duration-300"
                 >
                     Войти
                 </button>
                 <button
                     onClick={() => { setIsLoginForm(false); openModal(); }}
-                    className="px-4 py-2 border border-[#4400BA] bg-[#4400BA] font-bold text-white rounded-md"
+                    className="rounded-[25px] py-[0.5em] px-[2em] bg-[#4400BA] text-white text-[15px] font-bold border-[1px] border-[#4400BA] hover:bg-transparent hover:text-indigo-800 transition-all duration-300"
                 >
                     Регистрация
                 </button>
--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -2,8 +2,56 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center rounded-[22px]  h-[913px] shadow-[0_0_5px_1px_rgba(0,0,0,0.25)] bg-white">
+      <div className="flex items-center justify-between  w-[92%] ">
+      <div className="flex flex-col justify-between w-[40%]">
+        <div className="pb-[2em]">
+          <div className="w-[60%] pb-[1em]">
+            <h1 className=" text-[#000] text-[42px] font-bold whitespace-pre-wrap">Туристические 
+            маршруты по 
+            России с</h1>
+            <h1 className="text-[#4400BA] text-[45px] font-bold">GeoTouristicService</h1>
+          </div>
+          <p className="text-[#000] text-[18px] font-light">Прокладывай маршрут, изменяй приватность, делись с друзьями!</p>
+        </div>
+        <div className="pt-[1em] pb-[1em] flex items-center gap-[1em]">
+          <a 
+          className="rounded-[25px] py-[1em] px-[2em] bg-[#4400BA] text-white text-[15px] font-bold border-[1px] border-[#4400BA] hover:bg-transparent hover:text-indigo-800 transition-all duration-300" href=""
+          >Маршруты</a>
+          <a 
+          className="rounded-[25px] py-[1em] px-[2em] bg-[#4400BA] text-white text-[15px] font-bold border-[1px] border-[#4400BA] hover:bg-transparent hover:text-indigo-800 transition-all duration-300" href=""
+          >Маршруты рядом</a>
+        </div>
+      </div>
 
-    </>
+      <div className="flex flex-col gap-[6em] w-[40%]">
+        <div className="flex justify-between">
+          <div className="flex flex-col justify-between">
+            <img src="" alt="" className=" w-[300px] h-[200px]"/>
+            <img src="" alt="" className=" w-[300px] h-[200px]" />
+          </div>
+          <img src="" alt="" className="rounded-[36px] w-[242px] h-[461px]"/>
+        </div>
+
+        <div className="flex items-center justify-between rounded-[15px] p-[20px_17px] shadow-[0_4px_5px_2px_rgba(0,0,0,0.1)] bg-white">
+          <div className="flex flex-col items-start justify-center">
+            <h1 className="text-[#4400BA] text-[40px] font-bold">98%</h1>
+            <p className="text-[#000] text-[15px] font-medium">Clients satisfied</p>
+          </div>
+
+          <div className="flex flex-col items-start justify-center">
+            <h1 className="text-[#4400BA] text-[40px] font-bold">75</h1>
+            <p className="text-[#000] text-[15px] font-medium">Clients satisfied</p>
+          </div>
+
+          <div className="flex flex-col items-start justify-center">
+            <h1 className="text-[#4400BA] text-[40px] font-bold">104</h1>
+            <p className="text-[#000] text-[15px] font-medium">Clients satisfied</p>
+          </div>
+        </div>
+      </div>
+
+      </div>
+    </div>
   );
 }
