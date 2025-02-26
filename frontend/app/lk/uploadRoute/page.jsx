f7253e6cbf3eab1e359598f0dcd89259375b4c34
Kirillica228 <kirillicar8@gmail.com>
2025-02-26 18:18:22 +0400

    Проверка связи

--- frontend/app/lk/uploadRoute/page.jsx
+++ frontend/app/lk/uploadRoute/page.jsx
@@ -58,6 +58,7 @@ export default function uploadRoute() {
                                     </div>
                                 )
                             }
+                            {/* fffffffffffffffffffffff */}
                             
             </div>
         </>
