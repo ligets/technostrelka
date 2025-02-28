1f5cba31b1cb8c45286ab1976d112a1b1af58bc9
ligets <legen2a208@gmail.com>
2025-02-28 22:17:29 +0400

    asd

--- sh/install-node.sh
+++ sh/install-node.sh
@@ -3,11 +3,11 @@
 # Обновляем список пакетов
 sudo apt update
 
-# Устанавливаем необходимые зависимости
-sudo apt install -y curl gnupg
+# Устанавливаем curl, если он еще не установлен
+sudo apt install -y curl
 
-# Добавляем официальный репозиторий Node.js
-curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash -
+# Добавляем репозиторий для установки последней версии Node.js
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 
 # Устанавливаем Node.js и npm
 sudo apt install -y nodejs
